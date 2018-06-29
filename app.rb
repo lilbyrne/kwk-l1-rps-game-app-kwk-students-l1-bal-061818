8a12d80a90f41c3d6d3fb0b96b0441e1601c2649
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-29 14:01:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
-require_relative 'config/environment'
+require 'bundler'
+Bundler.require 
+require_relative 'models/ecogen'
 
-class GameApp < Sinatra::Base
-  get '/rps/:play' do
-    # Your app code goes here
-    # Don't worry about this UNTIL you've passed all the tests!
-    erb :rps_game
+class App < Sinatra::Base
+
+  # configure do
+  #   set :public_folder, 'public'
+  #   set :views, 'app/views'
+  # end
+
+  get '/' do
+    erb :ecogov
   end
-end
+  
+  get '/ecogov' do 
+   @state_name=params[state]
+    erb :senators
+  end 
+  
+  post '/results' do 
+    @senators=your_senators(@state_name)
+    erb :senators
+  end 
+end 
